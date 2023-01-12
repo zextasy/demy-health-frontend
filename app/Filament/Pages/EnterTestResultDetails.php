6d8a9a692f65097aa91099a5cdf9a82e2366dd66
Ugochukwu Ezekwem <>
2023-01-12 11:40:42 +0100

    save virtual data fields on enter result details

--- app/Filament/Pages/EnterTestResultDetails.php
+++ app/Filament/Pages/EnterTestResultDetails.php
@@ -3,7 +3,9 @@
 namespace App\Filament\Pages;
 
 use Filament\Pages\Page;
+use App\Models\TestResult;
 use App\Models\TestBooking;
+use Filament\Facades\Filament;
 use App\Models\TestResultTemplate;
 use Filament\Forms\Components\Hidden;
 use App\Services\VirtualFieldService;
@@ -11,11 +13,15 @@ use Filament\Forms\Contracts\HasForms;
 use Filament\Forms\Components\Section;
 use Filament\Forms\Components\Fieldset;
 use Filament\Forms\Components\TextInput;
+use App\Filament\Resources\TestResultResource;
 use Filament\Forms\Concerns\InteractsWithForms;
 
 class EnterTestResultDetails extends Page implements HasForms
 {
     use InteractsWithForms;
+
+    public $data;
+    private ?TestBooking $testBooking = null;
 
     protected static ?string $navigationIcon = 'heroicon-o-document-text';
 
@@ -26,10 +32,21 @@ class EnterTestResultDetails extends Page implements HasForms
         return false;
     }
 
+    public function mount(): void
+    {
+        $this->testBooking = TestBooking::findOrFail(request()->get('testBookingId'));
+        $this->form->fill([
+            'test_booking_id' => $this->testBooking->id,
+            'customer_email' => $this->testBooking->customer_email,
+            'customer_phone_number' => $this->testBooking->customer_phone_number,
+        ]);
+    }
+
+
     protected function getFormSchema(): array
     {
-        $testBooking = TestBooking::findOrFail(request()->get('testBookingId'));
-        $testResultTemplate = $testBooking->testType->testResultTemplate;
+
+        $testResultTemplate = $this->testBooking?->testType->testResultTemplate;
 
         $basicFields = [
             Hidden::make('test_booking_id'),
@@ -50,8 +67,25 @@ class EnterTestResultDetails extends Page implements HasForms
         return array_merge($basicFields, $extraFields);
     }
 
-    public function submit()
+    public function getCancelButtonUrlProperty()
     {
-        $this->form->getState();
+        return Filament::getUrl();
+    }
+
+    protected function getFormStatePath(): ?string
+    {
+        return 'data';
+    }
+
+    public function submit(): void
+    {
+        $testResult = new TestResult;
+        $testResult->has_template_data = true;
+        foreach ($this->data as $key => $value) {
+            $testResult->$key = $value;
+        }
+        $testResult->save();
+        $this->notify('success', 'The Details have been Saved.');
+        $this->redirect(TestResultResource::getUrl('view', ['record' => $testResult->id]));
     }
 }
