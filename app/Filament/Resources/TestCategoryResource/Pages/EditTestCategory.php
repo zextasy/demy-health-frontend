7616ed55d2314c12decb03f5055205d42ad851fd
William Onaghise <wonaghise@yahoo.com>
2023-09-13 14:39:35 +0100

    My Commit

--- app/Filament/Resources/TestCategoryResource/Pages/EditTestCategory.php
+++ app/Filament/Resources/TestCategoryResource/Pages/EditTestCategory.php
@@ -4,8 +4,17 @@ namespace App\Filament\Resources\TestCategoryResource\Pages;
 
 use App\Filament\Resources\TestCategoryResource;
 use Filament\Resources\Pages\EditRecord;
+use Filament\Pages\Actions;
 
 class EditTestCategory extends EditRecord
 {
     protected static string $resource = TestCategoryResource::class;
+
+    protected function getActions(): array
+    {
+        return [
+            Actions\ViewAction::make(),
+            Actions\DeleteAction::make(),
+        ];
+    }
 }
